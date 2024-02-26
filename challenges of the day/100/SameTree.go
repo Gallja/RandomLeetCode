6e7f019af6bb47e2bf13a6032242d7c52a899e79
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-02-26 16:24:14 +0100

    fix SameTree file

--- challenges of the day/100/SameTree.go
+++ challenges of the day/100/SameTree.go
@@ -14,43 +14,44 @@ type binaryTree struct {
 	root *TreeNode
 }
 
-// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-func isSameTree(p *TreeNode, q *TreeNode) bool {
-	pContent := recursiveDFSinorder(p)
-	qContent := recursiveDFSinorder(q)
-
-	if len(qContent) != len(pContent) {
-		return false
-	}
-
-	for i := 0; i < len(qContent); i++ {
-		if pContent[i] != qContent[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
 func newNode(value int) *TreeNode {
 	return &TreeNode{value, nil, nil}
 }
 
-func recursiveDFSinorder(root *TreeNode) []int {
-	var result []int
-
-	if root != nil {
-		// Visita il sottoalbero sinistro
-		result = append(result, recursiveDFSinorder(root.Left)...)
-
-		// Aggiungi il valore del nodo corrente
-		result = append(result, root.Val)
-
-		// Visita il sottoalbero destro
-		result = append(result, recursiveDFSinorder(root.Right)...)
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
 	}
 
-	return result
+	if p == nil || q == nil {
+		return false
+	}
+
+	if p.Left == nil && q.Left != nil {
+		return false
+	}
+
+	if p.Right == nil && q.Right != nil {
+		return false
+	}
+
+	// Confronta i valori dei nodi
+	if p.Val != q.Val {
+		return false
+	}
+
+	// Visita il sottoalbero sinistro
+	if !isSameTree(p.Left, q.Left) {
+		return false
+	}
+
+	// Visita il sottoalbero destro
+	if !isSameTree(p.Right, q.Right) {
+		return false
+	}
+
+	return true
 }
 
 func main() {
@@ -64,9 +65,6 @@ func main() {
 	tree2.root = newNode(1)
 	tree2.root.Left = newNode(2)
 	tree2.root.Right = newNode(3)
-
-	fmt.Println(recursiveDFSinorder(tree1.root))
-	fmt.Println(recursiveDFSinorder(tree2.root))
 
 	fmt.Println(isSameTree(tree1.root, tree2.root))
 }
