4b9602708061ebf95f57041f4cba6847d0c5b0ac
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-02-28 16:48:39 +0100

    update merge function

--- Top Interview 150/1/MergeSortedArray.go
+++ Top Interview 150/1/MergeSortedArray.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	newDim := n + m
+
+	nums1 = append(nums1, make([]int, newDim-len(nums1))...)
+	copy(nums1[len(nums1)-len(nums2):], nums2)
+}
+
 func main() {
-	fmt.Println("prova")
+	nums1 := []int{1, 3, 5, 16}
+	nums2 := []int{7, 14, 99, 101, 104}
+
+	merge(nums1, len(nums1), nums2, len(nums2))
+
+	fmt.Println(nums1)
 }
