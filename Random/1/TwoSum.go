9800baa2c992a5a66e8cd59a94353b77356461a9
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-02-28 20:23:05 +0100

    update Two Sum file

--- Random/1/TwoSum.go
+++ Random/1/TwoSum.go
@@ -5,14 +5,23 @@ import (
 )
 
 func twoSum(nums []int, target int) []int {
-	var ris []int
+	ris := make([]int, 0)
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				ris = append(ris, i)
+				ris = append(ris, j)
+			}
+		}
+	}
 
 	return ris
 }
 
 func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
+	nums := []int{3, 3}
+	target := 6
 
 	fmt.Println(twoSum(nums, target))
 }
