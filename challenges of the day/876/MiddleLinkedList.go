856756438a1de4aa77db9bea3ed7b05a56a2a1de
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-03-07 15:24:40 +0100

    add structs

---
+++ challenges of the day/876/MiddleLinkedList.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type linkedList struct {
+	head *ListNode
+	size int
+}
+
+func middleNode(head *ListNode) *ListNode {
+	return nil
+}
+
+func main() {
+	fmt.Println("prova")
+}
