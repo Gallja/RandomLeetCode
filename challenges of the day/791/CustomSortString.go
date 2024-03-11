aeca83c5d22fbe0b8d3035fc17aca9b4878a39a6
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-03-11 11:03:22 +0100

    update functions

--- challenges of the day/791/CustomSortString.go
+++ challenges of the day/791/CustomSortString.go
@@ -12,9 +12,42 @@ Permute the characters of s so that they match the order that order was sorted. 
 Return any permutation of s that satisfies this property.
 */
 func customSortString(order string, s string) string {
-	return ""
+	strOut := ""
+
+	for i := 0; i < len(order); i++ {
+		for j := 0; j < len(s); j++ {
+			if order[i] == s[j] {
+				strOut += string(order[i])
+			}
+		}
+	}
+
+	strOut += checkS(s, order)
+
+	return strOut
+}
+
+func checkS(str1, str2 string) string {
+	isIn := false
+	strOut := ""
+
+	for i := 0; i < len(str1); i++ {
+		isIn = false
+		for j := 0; j < len(str2); j++ {
+			if str1[i] == str2[j] {
+				isIn = true
+			}
+		}
+		if !isIn {
+			strOut += string(str1[i])
+		}
+	}
+
+	return strOut
 }
 
 func main() {
-	fmt.Println("prova")
+	fmt.Println(customSortString("cba", "cbad"))
+
+	fmt.Println(customSortString("bcafg", "abcd"))
 }
