6ef519c3c7e137b397a103ac892cca592d9983cf
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-03-14 11:26:10 +0100

    add new functions & update numSubarraysWithSum function

--- challenges of the day/930/BinarySubarraysSum.go
+++ challenges of the day/930/BinarySubarraysSum.go
@@ -10,11 +10,37 @@ Given a binary array nums and an integer goal, return the number of non-empty su
 A subarray is a contiguous part of the array.
 */
 func numSubarraysWithSum(nums []int, goal int) int {
-	return 0
+	curr := 0
+	ris := 0
+
+	for i := 0; i < len(nums); i++ {
+		curr += nums[i]
+
+		if curr == goal {
+			ris++
+		} else if curr == goal+1 {
+			break
+		}
+	}
+
+	return ris
+}
+
+func subArray(nums []int, goal int) int {
+	occorrenze := 0
+
+	for len(nums) != 0 {
+		occorrenze += numSubarraysWithSum(nums, goal)
+		nums = nums[1:]
+	}
+
+	return occorrenze
 }
 
 func main() {
-	nums := []int{1, 0, 1, 0, 1}
+	nums := []int{0, 0, 0, 0, 0}
+	fmt.Println(subArray(nums, 0))
 
-	fmt.Println(numSubarraysWithSum(nums, 2))
+	nums2 := []int{1, 0, 1, 0, 1}
+	fmt.Println(subArray(nums2, 2))
 }
