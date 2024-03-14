f1e4b0da1e2edbef88eb12c634e0339dccc529c9
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-03-14 10:13:20 +0100

    add doc

--- challenges of the day/930/BinarySubarraysSum.go
+++ challenges of the day/930/BinarySubarraysSum.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+/*
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+*/
+func numSubarraysWithSum(nums []int, goal int) int {
+	return 0
+}
+
 func main() {
-	fmt.Println("prova")
+	nums := []int{1, 0, 1, 0, 1}
+
+	fmt.Println(numSubarraysWithSum(nums, 2))
 }
