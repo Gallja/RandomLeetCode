1acda5db6acdcd9efad2ab4e5c8505592f729eca
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-04-22 23:55:43 +0200

    update code

--- challenges of the day/752/OpenTheLock.go
+++ challenges of the day/752/OpenTheLock.go
@@ -48,8 +48,8 @@ func newGraph() graph {
 	return graph{make(map[string][]string)}
 }
 
-func bfs(g graph, target string) (map[string]int, int) {
-	start := "0 0 0 0"
+func bfs(deadends []string, g graph, target string) (map[string]int, int) {
+	start := "0000"
 
 	queue := queue{nil}
 	distances := make(map[string]int)
@@ -63,13 +63,15 @@ func bfs(g graph, target string) (map[string]int, int) {
 
 		for _, v := range g.map_[u] {
 			if _, reached := distances[v]; !reached {
-				distances[v] = distances[u] + 1
+				if !contains(deadends, v) {
+					distances[v] = distances[u] + 1
 
-				if v == target {
-					val = distances[v]
+					if v == target {
+						val = distances[v]
+					}
+
+					queue.enqueue(v)
 				}
-
-				queue.enqueue(v)
 			}
 		}
 
@@ -78,12 +80,68 @@ func bfs(g graph, target string) (map[string]int, int) {
 	return distances, val
 }
 
+func contains(slice []string, val string) bool {
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == val {
+			return true
+		}
+	}
+
+	return false
+}
+
+func openLock(deadends []string, target string) int {
+	g := newGraph()
+
+	start := "0000"
+
+	queue := queue{nil}
+	distances := make(map[string]int)
+	distances[start] = 0
+
+	queue.enqueue(start)
+	val := 0
+
+	for !queue.isEmpty() {
+		u := queue.dequeue()
+
+		adjacent := searchAdjacent(u)
+
+		for _, v := adjacent {
+			if _, reached := distances[v]; !reached {
+				if !contains(deadends, v) {
+					distances[v] = distances[u] + 1
+
+					if v == target {
+						val = distances[v]
+					}
+
+					queue.enqueue(v)
+				}
+			}
+		}
+
+	}
+
+	return val
+}
+
+func searchAdjacent(start string) []string {
+
+	return nil
+}
+
 func main() {
 	graph := newGraph()
 
-	graph.map_["0 0 0 0"] = []string{"1 0 0 0", "0 1 0 0"}
-	graph.map_["1 0 0 0"] = []string{"2 0 0 0", "1 1 0 0"}
-	graph.map_["0 1 0 0"] = []string{"0 2 0 0", "0 1 1 0"}
+	graph.map_["0000"] = []string{"1000", "0100"}
+	graph.map_["1000"] = []string{"2000", "1100"}
+	graph.map_["0100"] = []string{"0200", "0110"}
+	graph.map_["0200"] = []string{"0300", "0000"}
+	graph.map_["0110"] = []string{"0000", "1000"}
 
-	fmt.Println(bfs(graph, "0 2 0 0"))
+	deadends := []string{"0201", "0101", "0102", "1212", "2002"}
+
+	fmt.Println(bfs(deadends, graph, "0300"))
+	fmt.Println(openLock(deadends, "0202"))
 }
