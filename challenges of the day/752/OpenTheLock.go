18c7b35196a3384878bf1d705cea1a558933a75d
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-04-22 16:24:17 +0200

    add queue's functions

--- challenges of the day/752/OpenTheLock.go
+++ challenges of the day/752/OpenTheLock.go
@@ -4,6 +4,49 @@ import (
 	"fmt"
 )
 
+type node struct {
+	next  *node
+	value []int
+}
+
+type queue struct {
+	head *node
+	last *node
+}
+
+func (q *queue) enqueue(value []int) {
+	newNode := &node{nil, value}
+
+	if q.head == nil && q.last == nil {
+		q.head = newNode
+		q.last = newNode
+		return
+	}
+
+	q.last.next = newNode
+	q.last = newNode
+}
+
+func (q *queue) dequeue() *node {
+	if q.head == nil && q.last == nil {
+		return nil
+	}
+
+	node := q.head
+	if q.head == q.last {
+		q.head = nil
+		q.last = nil
+	}
+
+	q.head = node.next
+	return node
+}
+
+func (q queue) isEmpty() bool {
+	return q.head == nil
+}
+
 func main() {
 	fmt.Println("prova")
+
 }
