30052b981c91a08c35409ebda3d15000b0a56bd9
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-04-26 16:55:20 +0200

    update main function

--- challenges of the day/1282/minimumFallingPathSum.go
+++ challenges of the day/1282/minimumFallingPathSum.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+func minFallingPathSum(grid [][]int) int {
+
+	return 0
+}
+
 func main() {
-	fmt.Println("prova")
+	grid := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	fmt.Println(minFallingPathSum(grid))
 }
