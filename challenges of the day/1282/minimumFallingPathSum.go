b10b05be5b69cead20e7a6338e745497fe9473d2
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-04-26 17:10:53 +0200

    update code

--- challenges of the day/1282/minimumFallingPathSum.go
+++ challenges of the day/1282/minimumFallingPathSum.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func minFallingPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
 
-	return 0
+	// Initialize the first row
+	for i := range grid[0] {
+		dp[0][i] = grid[0][i]
+	}
+
+	// Iterate through the grid from the second row
+	for y := 1; y < m; y++ {
+		for x := 0; x < n; x++ {
+			// Find the minimum sum from the previous row excluding the cell above
+			prevMin := math.MaxInt32
+			for i := 0; i < n; i++ {
+				if i != x {
+					prevMin = min(prevMin, dp[y-1][i])
+				}
+			}
+			dp[y][x] = grid[y][x] + prevMin
+		}
+	}
+
+	// Find the minimum element in the last row
+	minSum := math.MaxInt32
+	for _, val := range dp[m-1] {
+		minSum = min(minSum, val)
+	}
+	return minSum
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func main() {
