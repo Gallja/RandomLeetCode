d49f619d490d7223d4fbb7ac3b46e3b1b2f75e06
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-07 11:44:23 +0200

    add linkedList methods

--- challenges of the day/2816/doubleAnumber.go
+++ challenges of the day/2816/doubleAnumber.go
@@ -4,6 +4,63 @@ import (
 	"fmt"
 )
 
+type LinkedList struct {
+	head *ListNode
+	size int
+}
+
+type ListNode struct {
+	next  *ListNode
+	value int
+}
+
+func (list *LinkedList) add(value int) {
+	newNode := &ListNode{nil, value}
+
+	if list.head == nil {
+		list.head = newNode
+	} else {
+		current := list.head
+		for current.next != nil {
+			current = current.next
+		}
+
+		current.next = newNode
+	}
+
+	list.size++
+}
+
+func (list *LinkedList) printList() {
+	if list.size == 0 {
+		fmt.Println("[ ]")
+		return
+	}
+
+	fmt.Print("[ ", list.head.value)
+
+	current := list.head.next
+
+	for current != nil {
+		fmt.Print(", ", current.value)
+		current = current.next
+	}
+
+	fmt.Print(" ]")
+	fmt.Println()
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func doubleIt(head *ListNode) *ListNode {
+	return nil
+}
+
 func main() {
 	fmt.Println("prova")
 }
