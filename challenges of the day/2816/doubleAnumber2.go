9aa41b8f050b2bac472ff26db881bd3bda49dbc8
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-07 13:14:29 +0200

    update code

--- challenges of the day/2816/doubleAnumber2.go
+++ challenges of the day/2816/doubleAnumber2.go
@@ -51,46 +51,8 @@ func (list *LinkedList) printList() {
 }
 
 func doubleIt(head *ListNode) *ListNode {
-	if head.Val == 0 && head.Next == nil {
-		listRet := LinkedList{nil, 0}
-		listRet.add(0)
 
-		return listRet.head
-	}
-
-	nums := []int{}
-	var num int
-
-	current := head
-	cnt := 0
-
-	for current != nil {
-		nums = append(nums, current.Val)
-		current = current.Next
-		cnt++
-	}
-
-	fmt.Println(nums)
-
-	for _, Val := range nums {
-		num = num*10 + Val
-	}
-
-	fmt.Println(num)
-
-	num *= 2
-
-	fmt.Println(num)
-
-	slice := intToSlice(num)
-
-	listRet := LinkedList{nil, 0}
-
-	for i := 0; i < len(slice); i++ {
-		listRet.add(slice[i])
-	}
-
-	return listRet.head
+	return nil
 }
 
 func intToSlice(n int) []int {
@@ -112,33 +74,6 @@ func main() {
 	list.add(1)
 	list.add(9)
 	list.add(5)
-	/*
-		list.add(0)
-		list.add(5)
-		list.add(1)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-		list.add(9)
-	*/
 
 	list.printList()
 
