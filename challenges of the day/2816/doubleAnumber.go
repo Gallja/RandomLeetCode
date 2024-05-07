27923e2c6ed5289096ec19d8eb5f08506018af57
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-07 13:13:29 +0200

    update methods

--- challenges of the day/2816/doubleAnumber.go
+++ challenges of the day/2816/doubleAnumber.go
@@ -10,22 +10,22 @@ type LinkedList struct {
 }
 
 type ListNode struct {
-	next  *ListNode
-	value int
+	Val  int
+	Next *ListNode
 }
 
-func (list *LinkedList) add(value int) {
-	newNode := &ListNode{nil, value}
+func (list *LinkedList) add(Val int) {
+	newNode := &ListNode{Val, nil}
 
 	if list.head == nil {
 		list.head = newNode
 	} else {
 		current := list.head
-		for current.next != nil {
-			current = current.next
+		for current.Next != nil {
+			current = current.Next
 		}
 
-		current.next = newNode
+		current.Next = newNode
 	}
 
 	list.size++
@@ -37,30 +37,110 @@ func (list *LinkedList) printList() {
 		return
 	}
 
-	fmt.Print("[ ", list.head.value)
+	fmt.Print("[ ", list.head.Val)
 
-	current := list.head.next
+	current := list.head.Next
 
 	for current != nil {
-		fmt.Print(", ", current.value)
-		current = current.next
+		fmt.Print(", ", current.Val)
+		current = current.Next
 	}
 
 	fmt.Print(" ]")
 	fmt.Println()
 }
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func doubleIt(head *ListNode) *ListNode {
-	return nil
+	if head.Val == 0 && head.Next == nil {
+		listRet := LinkedList{nil, 0}
+		listRet.add(0)
+
+		return listRet.head
+	}
+
+	nums := []int{}
+	var num int
+
+	current := head
+	cnt := 0
+
+	for current != nil {
+		nums = append(nums, current.Val)
+		current = current.Next
+		cnt++
+	}
+
+	fmt.Println(nums)
+
+	for _, Val := range nums {
+		num = num*10 + Val
+	}
+
+	fmt.Println(num)
+
+	num *= 2
+
+	fmt.Println(num)
+
+	slice := intToSlice(num)
+
+	listRet := LinkedList{nil, 0}
+
+	for i := 0; i < len(slice); i++ {
+		listRet.add(slice[i])
+	}
+
+	return listRet.head
+}
+
+func intToSlice(n int) []int {
+	var result []int
+
+	for n > 0 {
+		digit := n % 10
+		result = append([]int{digit}, result...)
+		n /= 10
+	}
+
+	return result
 }
 
 func main() {
-	fmt.Println("prova")
+	list := LinkedList{nil, 0}
+
+	list.add(9)
+	list.add(1)
+	list.add(9)
+	list.add(5)
+	/*
+		list.add(0)
+		list.add(5)
+		list.add(1)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+		list.add(9)
+	*/
+
+	list.printList()
+
+	fmt.Println(doubleIt(list.head))
 }
