744ecb8996008d01e8d20858147738876dbfefb6
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-10 17:26:50 +0200

    add new challenge

---
+++ challenges of the day/786/SmallestPrimeFraction.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func kthSmallestPrimeFraction(arr []int, k int) []int {
+	return nil
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(kthSmallestPrimeFraction(arr, 3))
+}
