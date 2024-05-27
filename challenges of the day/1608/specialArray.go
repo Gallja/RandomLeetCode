49dad35d3ed7b6c9c341f44c8a54d1452a55d4fe
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-27 10:56:35 +0200

    update code

--- challenges of the day/1608/specialArray.go
+++ challenges of the day/1608/specialArray.go
@@ -4,6 +4,38 @@ import (
 	"fmt"
 )
 
+func specialArray(nums []int) int {
+	x := 0
+	special := true
+
+	for i := 0; i < len(nums); i++ {
+
+		for j := 0; j < i; j++ {
+			if nums[i] < i {
+				special = false
+				break
+			}
+		}
+
+		if special {
+			x = i + 1
+		}
+	}
+
+	if x == 0 {
+		return -1
+	}
+
+	return x
+}
+
 func main() {
-	fmt.Println("Prova")
+	nums := []int{3, 5}
+	fmt.Println(specialArray(nums))
+
+	nums2 := []int{0, 0}
+	fmt.Println(specialArray(nums2))
+
+	nums3 := []int{0, 4, 3, 0, 4}
+	fmt.Println(specialArray(nums3))
 }
