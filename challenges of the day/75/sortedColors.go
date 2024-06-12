ae733ad6dcff58f2a5182570902bf7278f226e08
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-12 10:38:27 +0200

    add new challenge

---
+++ challenges of the day/75/sortedColors.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sortColors(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	half := len(nums) / 2
+	left := make([]int, half)
+	right := make([]int, len(nums)-half)
+
+	copy(left, (nums)[:half])
+	copy(right, (nums)[half:])
+
+	sortColors(left)
+	sortColors(right)
+
+	merged := merge(left, right)
+	for i := 0; i < len(nums); i++ {
+		(nums)[i] = merged[i]
+	}
+}
+
+func merge(left, right []int) []int {
+	sorted := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			sorted = append(sorted, left[i])
+			i++
+		} else {
+			sorted = append(sorted, right[j])
+			j++
+		}
+	}
+
+	sorted = append(sorted, left[i:]...)
+	sorted = append(sorted, right[j:]...)
+
+	return sorted
+}
+
+func main() {
+	nums := []int{2, 0, 2, 1, 1, 0}
+
+	fmt.Println(sortColors(nums))
+}
