773c633ca7cb4ee9ebba47c1d533b120d7fbb75e
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-13 15:12:23 +0200

    add array sorting

--- challenges of the day/2037/seatEveryone.go
+++ challenges of the day/2037/seatEveryone.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+func minMovesToSeat(seats []int, students []int) int {
+	moves := 0
+
+	sort.Slice(seats, func(i, j int) bool {
+		return seats[i] < seats[j]
+	})
+	sort.Slice(students, func(i, j int) bool {
+		return students[i] < students[j]
+	})
+
+	return moves
+}
+
 func main() {
-	fmt.Println("new challenge")
+	seats := []int{3, 1, 5}
+	students := []int{2, 7, 4}
+
+	fmt.Println(minMovesToSeat(seats, students))
 }
