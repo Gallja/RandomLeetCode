726c7d2cf16c7d77b983df3b328e74d559acb19b
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-14 10:26:42 +0200

    update code

--- challenges of the day/945/minimumIncrement.go
+++ challenges of the day/945/minimumIncrement.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func minIncrementForUnique(nums []int) int {
-	return 0
+	moves := 0
+
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				nums[j]++
+				moves++
+			}
+		}
+	}
+
+	return moves
 }
 
 func main() {
