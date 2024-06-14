2c3173304d822db15dad7e1e51c0921e3426f677
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-14 10:38:18 +0200

    add new versione of minimumIncrement function

--- challenges of the day/945/minimumIncrement.go
+++ challenges of the day/945/minimumIncrement.go
@@ -24,12 +24,41 @@ func minIncrementForUnique(nums []int) int {
 	return moves
 }
 
+func minIncrementForUnique2(nums []int) int {
+	moves := 0
+
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	fmt.Println(nums)
+
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] >= nums[i+1] {
+			nums[i+1]++
+			moves++
+			if nums[i] == nums[i+1] {
+				nums[i+1]++
+				moves++
+			}
+		}
+	}
+
+	fmt.Println(nums)
+
+	return moves
+}
+
 func main() {
 	nums := []int{1, 2, 2}
-
 	fmt.Println(minIncrementForUnique(nums))
 
 	nums2 := []int{3, 2, 1, 2, 1, 7}
-
 	fmt.Println(minIncrementForUnique(nums2))
+
+	nums3 := []int{3, 2, 1, 2, 1, 7}
+	fmt.Println(minIncrementForUnique2(nums3))
+
+	nums4 := []int{1, 2, 2}
+	fmt.Println(minIncrementForUnique2(nums4))
 }
