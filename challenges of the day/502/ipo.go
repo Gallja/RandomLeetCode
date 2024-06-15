55c58456aa8305ee5af4ee514c2e5716acf67761
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-15 16:04:32 +0200

    update code

--- challenges of the day/502/ipo.go
+++ challenges of the day/502/ipo.go
@@ -4,6 +4,61 @@ import (
 	"fmt"
 )
 
+type coppia struct {
+	primo   int
+	secondo int
+}
+
+type heap []int
+
+func lenHeap(maxHeap heap) int {
+	return len(maxHeap)
+}
+
+func push(maxHeap *heap, x int) {
+	*maxHeap = append(*maxHeap, x)
+}
+
+func pop(maxheap *heap) int {
+	bkcp := *maxheap
+	len := len(*maxheap)
+
+	last := bkcp[len-1]
+	*maxheap = bkcp[0 : len-1]
+
+	return last
+}
+
+func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
+	coppie := make([]coppia, len(profits))
+	maxHeap := &heap{}
+
+	for i := 0; i < len(profits); i++ {
+		coppie[i] = coppia{capital[i], profits[i]}
+	}
+
+	for k > 0 {
+		for j := 0; j < len(profits) && w >= coppie[j].primo; j++ {
+			push(maxHeap, coppie[j].secondo)
+		}
+
+		if lenHeap(*maxHeap) == 0 {
+			break
+		}
+
+		w += pop(maxHeap)
+		k--
+	}
+
+	return w
+}
+
 func main() {
-	fmt.Println("new challenge")
+	k := 2
+	w := 0
+	profits := []int{1, 2, 3}
+	capital := []int{0, 1, 1}
+
+	fmt.Println(findMaximizedCapital(k, w, profits, capital))
+
 }
