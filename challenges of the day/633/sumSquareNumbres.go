cc3fe2723f4477191b56d42e7b6376b744a10b4d
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-17 10:11:26 +0200

    add new version of judgeSquareSum function

--- challenges of the day/633/sumSquareNumbres.go
+++ challenges of the day/633/sumSquareNumbres.go
@@ -6,7 +6,6 @@ import (
 )
 
 func judgeSquareSum(c int) bool {
-	squareOk := false
 	cnt := 0
 	num2 := cnt + 1
 
@@ -24,15 +23,37 @@ func judgeSquareSum(c int) bool {
 		cnt++
 	}
 
-	return squareOk
+	return false
+}
+
+func judgeSquareSum2(c int) bool {
+	left, right := 0, int(math.Sqrt(float64(c)))
+
+	for left <= right {
+		sum := left*left + right*right
+
+		if sum == c {
+			return true
+		} else if sum > c {
+			right--
+		} else {
+			left++
+		}
+	}
+
+	return false
 }
 
 func main() {
 	fmt.Println(judgeSquareSum(5))
-
 	fmt.Println(judgeSquareSum(3))
-
 	fmt.Println(judgeSquareSum(4))
-
 	fmt.Println(judgeSquareSum(2))
+
+	fmt.Println("Versione 2")
+
+	fmt.Println(judgeSquareSum2(5))
+	fmt.Println(judgeSquareSum2(3))
+	fmt.Println(judgeSquareSum2(4))
+	fmt.Println(judgeSquareSum2(2))
 }
