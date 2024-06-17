eaf7994eda97e952a89ae11068eb41023e7967d5
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-06-17 09:44:16 +0200

    update judgeSquadreSum function

--- challenges of the day/633/sumSquareNumbres.go
+++ challenges of the day/633/sumSquareNumbres.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func judgeSquareSum(c int) bool {
-	return true
+	squareOk := false
+	cnt := 1
+	num2 := cnt + 1
+
+	for cnt < c {
+		num2 = cnt + 1
+
+		for num2 < c {
+			if math.Pow(float64(cnt), 2)+math.Pow(float64(num2), 2) == float64(c) {
+				return true
+			}
+			// fmt.Println(math.Pow(float64(cnt), 2) + math.Pow(float64(num2), 2))
+
+			num2++
+		}
+
+		cnt++
+	}
+
+	return squareOk
 }
 
 func main() {
-	fmt.Println("New Challenge")
+	fmt.Println(judgeSquareSum(5))
 }
