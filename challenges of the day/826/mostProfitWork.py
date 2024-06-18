966e8642d563c3363046daa2938b748e4795bdeb
Gallja <andrea.galliano2@studenti.unimi.it>
2024-06-18 09:53:12 +0200

    update code

--- challenges of the day/826/mostProfitWork.py
+++ challenges of the day/826/mostProfitWork.py
@@ -1,22 +1,33 @@
 class Solution(object):
     def maxProfitAssignment(self, difficulty, profit, worker):
-        """
-        :type difficulty: List[int]
-        :type profit: List[int]
-        :type worker: List[int]
-        :rtype: int
-        """
-        
+        worker.sort()
 
-class Coppia:
-    def __init__(self, difficulty, profit):
-        self.difficulty = difficulty
-        self.profit = profit
+        coppie = list(zip(difficulty, profit))
+        coppie.sort(key=lambda x : x[0])
 
+        worker.sort()
+
+        '''
+        for i in range(len(coppie)):
+            for j in range(len(worker)):
+        '''
+
+        return coppie, worker
+
+
+def generaCoppie(difficulty, profit):
+    coppie = []
+
+    for i in range(len(difficulty)):
+        coppie.append(Coppia(difficulty[i], profit[i]))
+
+    return coppie
 
 difficulty = [2, 4, 6, 8, 10]
 profit = [10, 20, 30, 40, 50]
 worker = [4, 5, 6, 7]
 
 solution = Solution()
-result = solution.maxProfitAssignment(difficulty, profit, worker)
+res1, res2 = solution.maxProfitAssignment(difficulty, profit, worker)
+print(res1)
+print(res2)
