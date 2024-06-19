2d4cc249cb475597c4e1ce4f82f707f3d884fbf0
Gallja <andrea.galliano2@studenti.unimi.it>
2024-06-19 09:16:32 +0200

    add new challenge

---
+++ challenges of the day/1482/minimumNumberDaysBouquets.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        
+        return 0
+    
+
+solution = Solution()
+bloomDay = [1, 10, 3, 10, 2]
+m = 3
+k = 1
+print(solution.minDays(bloomDay, m, k))
