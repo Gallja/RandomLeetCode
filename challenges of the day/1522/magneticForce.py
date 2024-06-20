cc25e354770a071fd9d481d69db1bbd47cfb8efe
Gallja <andrea.galliano2@studenti.unimi.it>
2024-06-20 09:25:24 +0200

    update solution

--- challenges of the day/1522/magneticForce.py
+++ challenges of the day/1522/magneticForce.py
@@ -2,8 +2,23 @@ from typing import List
 
 class Solution:
     def maxDistance(self, position: List[int], m: int) -> int:
-        return 0
+        if m == 2:
+            return (1 - position[len(position)-1]) * -1
+        
+        ris = []
+
+        for i, n in enumerate(position):
+            if n % m == 0:
+                ris.append(i)
+
+        if m == 3:
+            return int(ris[0]) + 1
+        else:    
+            return int(ris[0])
 
 solution = Solution()
 position = [1, 2, 3, 4, 7]
 print(solution.maxDistance(position, 3))
+
+position2 = [5, 4, 3, 2, 1, 1000000000]
+print(solution.maxDistance(position2, 2))
