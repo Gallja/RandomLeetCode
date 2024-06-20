65ea7df57f997cf48af2d9e56b35cbd5f9ecaeac
Gallja <andrea.galliano2@studenti.unimi.it>
2024-06-20 09:17:33 +0200

    add new challenge

---
+++ challenges of the day/1522/magneticForce.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        return 0
+
+solution = Solution()
+position = [1, 2, 3, 4, 7]
+print(solution.maxDistance(position, 3))
