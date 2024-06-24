c4324efb2213836193e98537b9d15e947719b751
Gallja <andrea.galliano2@studenti.unimi.it>
2024-06-24 16:50:19 +0200

    add switch case

--- Random/3/romanToInt.py
+++ Random/3/romanToInt.py
@@ -1,4 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        def switch(car) -> int:
+            if car == 'I':
+                return 1
+            elif car == 'V':
+                return 5
+            elif car == 'X':
+                return 10
+            elif car == 'L':
+                return 50
+            elif car == 'C':
+                return 100
+            elif car == 'D':
+                return 500
+            elif car == 'M':
+                return 1000
+
         return 0
     
+solution = Solution()    
+print(solution.romanToInt('III'))
